0201ae45767b24b1a1414728afb07a7c15cbccd1
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-08-30 15:59:24 +0200

    Added pythonScriptsAPI

--- API/pythonScriptsAPI.py
+++ API/pythonScriptsAPI.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import core.linkero as linkero
-#import submodules.pythonScripts.alert_viewer as alerts
-import submodules.pythonScripts.modules.alert_viewer as alerts
+import submodules.pythonScripts.modules.alert_viewer as av
 
-
-class alert(linkero.Resource):
+class alerts(linkero.Resource):
     @linkero.auth.login_required
     def get(self):
-        alerts.alertViewer()
-        return "hi!"
+        filters = []
+        filters.append("[todo]")
+        numItems = av.countLines(av.openLog())
+        alerts = av.searchLastEntries(av.openLog(), filters, numItems)
+        for alert in alerts:
+            alert[0]=str(alert[0]).replace('\n', '')
+        return alerts
+
+class alertsByUser(linkero.Resource):
+    @linkero.auth.login_required
+    def get(self, user):
+        filters = []
+        filters.append("[info]")
+        filters.append(user)
+        numItems = 500
+        alerts = av.searchLastEntries(av.openLog(), filters, numItems)
+        for alert in alerts:
+            alert[0] = str(alert[0]).replace('\n', '')
+        return alerts
+
+class alertsByUserAndMonth(linkero.Resource):
+    @linkero.auth.login_required
+    def get(self, user, month):
+        filters = []
+        filters.append("[info]")
+        filters.append(user)
+        filters.append(month)
+        numItems = 500
+        alerts = av.searchLastEntries(av.openLog(), filters, numItems)
+        for alert in alerts:
+            alert[0] = str(alert[0]).replace('\n', '')
+        return alerts
 
 ##
 ## Actually setup the Api resource routing here
 ##
 def loadPythonScriptsAPI():
-    linkero.api.add_resource(alert, '/alert')
+    linkero.api.add_resource(alerts, '/alerts')
+    linkero.api.add_resource(alertsByUser, '/alerts/<user>')
+    linkero.api.add_resource(alertsByUserAndMonth, '/alerts/<user>&<month>')
